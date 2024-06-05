5b8e746072282fbd4850e897ead42115ebb50e4c
GerardoOli <122970775+GerardoOli@users.noreply.github.com>
2024-06-05 15:49:50 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def home():
     <head><title>Información Personal</title></head>
     <body style="font-family: Arial, sans-serif; background-color: #f0f8ff; text-align: center; padding: 20px;">
         <div style="background-color: #fff; padding: 20px; border-radius: 10px; display: inline-block;">
-            <h1 style="color: #333;">INGENIERÍA EN DESARROLLO Y GESTIÓN DE SOFTWARE </h1>
-            <h4 style="color: #333;">Gerardo Olivares Aguilar </h4>
-            <p style="color: #333;">Grado: 9 Grupo: B</p>
+            <h2 style="color: #333;">INGENIERÍA EN DESARROLLO Y GESTIÓN DE SOFTWARE </h1>
+            <h3 style="color: #333;">Gerardo Olivares Aguilar </h3>
+            <h3 style="color: #333;">Grado: 9 Grupo: B</h3>
         </div>
     </body>
     </html>
