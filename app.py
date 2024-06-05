2e459fb76ca49e55cdd8ad6b062fd18f66fb8bad
GerardoOli <122970775+GerardoOli@users.noreply.github.com>
2024-06-05 16:02:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ def home():
     <head><title>Información Personal</title></head>
     <body style="font-family: Arial, sans-serif; background-color: #f0f8ff; text-align: center; padding: 20px;">
         <div style="background-color: #fff; padding: 20px; border-radius: 10px; display: inline-block;">
-            <h2 style="color: #333;">INGENIERÍA EN DESARROLLO Y GESTIÓN DE SOFTWARE </h1>
-            <h3 style="color: #333;">Gerardo Olivares Aguilar </h3>
+            <img src="https://img.freepik.com/vector-premium/icono-perfil-usuario-estilo-plano-ilustracion-vector-avatar-miembro-sobre-fondo-aislado-concepto-negocio-signo-permiso-humano_157943-15752.jpg" alt="Foto de Perfil" style="border-radius: 50%; width: 150px; height: 150px; margin-bottom: 20px;">
+            <h3 style="color: #333;">Gerardo Olivares Aguilar</h3>
             <h3 style="color: #333;">Grado: 9 Grupo: B</h3>
         </div>
     </body>
